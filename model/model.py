9f17256702706fcdaa61932983610c5a9f34b098
João Vítor Bruno <joaovitor.bs2@hotmail.com>
2025-04-16 19:38:48 -0300

    Atualizando model e gerando arquivo SAV

--- model/model.py
+++ model/model.py
@@ -3,10 +3,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
+import joblib 
 
 # Importando base de dados
-#df = pd.read_csv("/content/drive/MyDrive/customer_churn_tratado.csv/part-00000-95b6e889-6a8a-4d27-87d5-2c2dd575c3ca-c000.csv")
-
+df = pd.read_csv("C:/Users/Jão/Downloads/churn.csv", delimiter=",")
 df = df.drop(columns=["customerID", "TotalCharges"], errors='ignore')
 df
 
@@ -23,8 +23,9 @@ model.fit(X_train, y_train)
 
 # Avaliar o modelo
 y_pred = model.predict(X_test)
+
 #print("Accuracy:", accuracy_score(y_test, y_pred))
 #print("\nClassification Report:\n", classification_report(y_test, y_pred))
 
-
+joblib.dump(model, "chur.sav")
 
